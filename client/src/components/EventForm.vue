1622728a5d568cbea770269e3a7d1448c241a0b0
Krista Prenesti <kprenesti@gmail.com>
2019-03-22 16:41:07 -0400

    ..And actually committing the eventForm as it is right now

---
+++ client/src/components/EventForm.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <div class="interests">
+      <label for="interests">Interests</label>
+      <input
+        type="text"
+        v-model="interests"
+        name="interests"
+        id="interests"
+        placeholder="Ex: JavaScript, HTML, CSS"
+      >
+    </div>
+    <div class="zip">
+      <label for="zip">Zipcode</label>
+      <input type="text" name="zip" v-model="zip" id="zip" placeholder="Ex: 30062">
+    </div>
+    <div class="radius">
+      <label for="radius">Radius (in miles)</label>
+      <input type="text" v-model="radius" name="radius" id="radius" placeholder="Ex: 10">
+    </div>
+    <button type="submit" @click.stop.prevent="handleSubmit">Submit</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      input: {
+        interests: "",
+        zip: "",
+        radius: ""
+      }
+    };
+  },
+  methods: {
+    handleSubmit(event) {}
+  }
+};
+</script>
+
+<style scoped>
+</style>
